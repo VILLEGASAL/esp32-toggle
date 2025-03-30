767b965696449895c8379ddaa295b2d58377d128
unknown <villegasalrandolph@gmail.com>
2025-03-30 16:55:23 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
+const PORT = 5000;
+
+let led_state = "0";
+
+app.use(cors({
+
+    origin: ["*"],   
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+app.get("/toggle", (req, res) => {
+
+    if (led_state === "0") {
+        
+        led_state = "1";
+
+    }else{
+
+        led_state = "0"
+    }
+
+    res.status(200).send(led_state);
+});
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server is running on port ${PORT}...`);
+    
+});
