f2d48d897a4b5f25958b02d378fe8c2972a608ce
unknown <villegasalrandolph@gmail.com>
2025-03-30 17:09:29 +0800

    Changed Again The CORS Config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let led_state = "0";
 
 app.use(cors({
 
-    origin: ["http://localhost:5500"],   
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],   
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
